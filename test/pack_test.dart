f0094a7f587576429920fd192e5dbb22a2ecb546
Artem IG <ortemeo@gmail.com>
2022-10-09 02:50:19 +0300

    upd

--- test/pack_test.dart
+++ test/pack_test.dart
@@ -57,15 +57,4 @@ void main() {
     expect(tempDir!.listSync().map((e) => path.basename(e.path)).toList(),
         ['abc_windows_amd64.zip']);
   });
-
-  test("detecting architecture", () {
-    // expect(detectArchitecture(File("test/executables/ghcp_linux_amd64")).string,
-    //     'linux_amd64');
-    // expect(detectArchitecture(File("test/executables/ghcp_osx_amd64")).string,
-    //     'macos_amd64');
-    // expect(detectArchitecture(File("test/executables/ghcp_windows_amd64")).string,
-    //     'windows_amd64');
-    // expect(()=>detectArchitecture(File("test/executables/labuda.txt")),
-    //     throwsA(isA<UnknownArchitectureException>()));
-  });
 }
